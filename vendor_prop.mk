8fddf40141056695d8760fa2fb3947d816e688d6
clarencelol <clarencekuiek@icloud.com>
2021-10-24 22:20:02 +0800

    lavender: prop: Disable disable_backpressure
    * debug.sf.disable_backpressure is bad according to https://android.googlesource.com/platform/frameworks/native/+/c5da271
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    [ImSpiDy: we have already enabled debug.sf.enable_gl_backpressure to improve our hwc]
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -246,7 +246,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.disable_backpressure=1 \
+    debug.sf.disable_backpressure=0 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
