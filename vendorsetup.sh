b5e4b86eca395dc9502c81b85438bed1d066e881
MRMDprjkt° <87246800+Merma1dmanPrjct@users.noreply.github.com>
2022-08-19 03:15:36 +0700

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone --depth=1  https://github.com/Merma1dmanPrjct/MRMD_KERNEL_XIAOMI_LAVENDER-OC -b mrmd-65hz kernel/xiaomi/lavender
+git clone --depth=1 https://github.com/Merma1dmanPrjct/vendor_xiaomi_lavender-nad vendor/xiaomi/lavender
