d018fe22a7420e129f6739cadb97d454f0701bde
xd-build <111744107+xd-build@users.noreply.github.com>
2022-08-23 20:58:54 +0600

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-git clone --depth=1  https://github.com/Merma1dmanPrjct/MRMD_KERNEL_XIAOMI_LAVENDER-OC -b mrmd-65hz kernel/xiaomi/lavender
-git clone --depth=1 https://github.com/Merma1dmanPrjct/vendor_xiaomi_lavender-nad vendor/xiaomi/lavender
