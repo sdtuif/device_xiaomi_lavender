1e4b04a61e37f220bcdca0949e78e39c66425ca0
Sa-Sajjad <111744107+Sa-Sajjad@users.noreply.github.com>
2022-08-27 08:35:35 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_lavender.mk
+    $(LOCAL_DIR)/du_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_lavender-user \
-    nad_lavender-userdebug \
-    nad_lavender-eng
+    du_lavender-user \
+    du_lavender-userdebug \
+    du_lavender-eng
